cc7c60e1e9d288eaf9ad52777e1d33ade9c49cfe
luishs0 <luishs.doc@gmail.com>
2022-10-14 13:22:44 +0200

    scaffolding

---
+++ js/script.js
@@ -0,0 +1,11 @@
+// Chiedere all'utente di inserire una parola
+// Creare una funzione per sapere se la parola inserita é palindromo
+
+let userWord = prompt("Dimmi la tua parola");
+console.log(userWord, typeof(userWord));
+
+// function (userWord) { il return mi deve dire true o false } se é palindromo = true, altrimenti false
+    // Identificare se é un palindromo oppure no
+        // descomporre la parola a lettere 
+        // mettere le lettere al contrario in una stringa formando una variabile nuova
+        // let parolaNormale = userWord; let parolaStorta = parola al contrario -> Confrontarli in un if
